87b14bc8e2587f21c6a5c570add751d60252be55
Hajime Furukawa <hajpoj@gmail.com>
2013-01-15 00:45:20 -0800

    backboneify

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ window.hf = {
   collections: {}
 };
 
+_.templateSettings = {
+  interpolate : /\{\{(.+?)\}\}/g
+};
+
 $(function() {
   var resources = [
     {
@@ -15,16 +19,90 @@ $(function() {
     },
     {
       name: "Underscore.js",
-      main_url: "http://www.underscorejs.org",
-      api_url: "http://www.underscorejs.org",
+      main_url: "http://underscorejs.org",
+      api_url: "http://underscorejs.org",
       code_url: "https://gist.github.com/4de2ec66ad46e4f521ef",
       github_url: "https://github.com/documentcloud/underscore"
+    },
+    {
+      name: "Underscore.string.js",
+      main_url: "http://epeli.github.com/underscore.string/",
+      api_url: "https://github.com/epeli/underscore.string#readme",
+      code_url: "https://gist.github.com/29d89f78de3f52da3c5b",
+      github_url: "https://github.com/epeli/underscore.string"
+    },
+    {
+      name: "JQuery",
+      main_url: "http://jquery.com/",
+      api_url: "http://api.jquery.com/",
+      code_url: "https://github.com/jquery/jquery/tree/master/src",
+      github_url: "https://github.com/jquery/jquery"
+    },
+    {
+      name: "Twitter Bootstrap",
+      main_url: "http://twitter.github.com/bootstrap/",
+      api_url: "http://twitter.github.com/bootstrap/javascript.html",
+      code_url: "https://github.com/twitter/bootstrap/blob/master/docs/assets/js/bootstrap.js",
+      github_url: "https://github.com/twitter/bootstrap"
     }
+//    , {
+//      name: "",
+//      main_url: "",
+//      api_url: "",
+//      code_url: "",
+//      github_url: ""
+//    }
   ];
 
-  hf.models.Resource = Backbone.Model.extend({});
-  hf.collections.Resources = Backbone.Collection.extend();
-  hf.views.Resource = Backbone.View.extend();
+  hf.models.Resource = Backbone.Model.extend();
 
+  hf.collections.Resources = Backbone.Collection.extend({
+    model: hf.models.Resource
+  });
+
+  hf.views.Resource = Backbone.View.extend({
+    initialize: function(options) {
+      this.template = _.template(options.template.html());
+    },
+
+    render: function() {
+      //remember the underscore templates take A JSON object NOT the model itself.s
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  hf.views.ResourceTable = Backbone.View.extend({
+
+    initialize: function(options) {
+      this.collection = new hf.collections.Resources(options.resourceJSON);
+      this.render();
+    },
+
+    render: function() {
+      var that = this;
+      this.collection.each(function(model) {
+        var resourceView = new hf.views.Resource({
+          template: $('#resourceTemplate'),
+          model: model,
+          tagName: 'tr'
+        });
+        that.$el.append(resourceView.render().el);
+      });
+    }
+  });
+
+  hf.views.Appview = Backbone.View.extend({
+    initialize: function() {
+      var resourceTableView = new hf.views.ResourceTable({
+        el: $('#resourceTable'),
+        resourceJSON: resources
+      });
+    }
+  });
+
+  var appView = new hf.views.Appview({
+    el: $('body')
+  });
 
 });
